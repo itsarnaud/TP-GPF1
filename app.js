3e5b0ba67dae71e9bcf85682b235e31d9b7eef75
Arnaud R. <arno.royer2004@gmail.com>
2026-05-26 11:44:24 +0200

    feat: init express and add sonar

---
+++ app.js
@@ -0,0 +1,35 @@
+import createError  from 'http-errors';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import path    from 'node:path';
+import logger  from 'morgan';
+import { fileURLToPath } from 'node:url';
+
+import router from './routes/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', router);
+
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.status(200).json({ success: true });
+});
+
+export default router;
