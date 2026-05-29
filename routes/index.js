91d07675e5e0b74b8d95c480af32f645d1dfbdcd
Arnaud R. <arno.royer2004@gmail.com>
2026-05-29 17:11:02 +0200

    feat: add spectators creation and list

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 import grandstand from './grandstand.js';
 import session from './session.js';
+import spectator from './spectator.js';
 
 router.use('/grandstands', grandstand);
 router.use('/sessions', session);
+router.use('/spectators', spectator);
 
 router.get('/healthz', (req, res) => {
   res.status(200).json({ status: 'ok' });
